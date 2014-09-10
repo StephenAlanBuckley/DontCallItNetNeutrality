2209530c6466a57e869ee8068d82b11f54ae371a
Stephen Buckley <stephen.alan.buckley@gmail.com>
2014-09-10 12:06:08 -0400

    adding the meat of the extension

---
+++ README.md
@@ -0,0 +1,6 @@
+Don't Call It Net Neutrality
+========
+
+Call it what it is!
+
+In accordance with John Oliver's Last Week Tonight's segment about Protecting Net Neutrality, and in honor of Net Neutrality Day, I present a Chrome extension replacing "Protecting Net Neutrality" with "Preventing Cable Company Fuckery."
---
+++ net_neutrality.js
@@ -0,0 +1,38 @@
+walk(document.body);
+
+function walk(node) 
+{
+    // I stole this function from here:
+    // http://is.gd/mwZp7E
+    
+    var child, next;
+
+    switch ( node.nodeType )  
+    {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while ( child ) 
+            {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+
+        case 3: // Text node
+            handleText(node);
+            break;
+    }
+}
+
+function handleText(textNode) 
+{
+    var value = textNode.nodeValue;
+
+    var pattern = new RegExp("/Protect([\S]*) Net Neutrality/", "gi", "Prevent$1 Cable Company Fuckery");
+    var new_value = value.replace(pattern);
+    
+    textNode.nodeValue = v;
+}
